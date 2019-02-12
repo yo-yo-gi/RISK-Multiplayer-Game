ffb52a16a1f11205067e7ed2fe3d117d73dfaf63
Chirag3112 <chirag123192@gmail.com>
2019-02-12 00:33:38 -0500

    Javadoc addition

--- Risk/src.main.java/com/soen/risk/controller/package-info.java
+++ Risk/src.main.java/com/soen/risk/controller/package-info.java
@@ -2,7 +2,10 @@
  * 
  */
 /**
- * @author Yogesh Nimbhorkar
- *
- */
+* The Class RiskController contains the functionality of the control flow of Risk Game. 
+*
+* @author Chirag
+* @version 1.0
+*/
 package com.soen.risk.controller;
+
--- Risk/src.test.java/com/soen/risk/test/controller/package-info.java
+++ Risk/src.test.java/com/soen/risk/test/controller/package-info.java
@@ -2,7 +2,9 @@
  * 
  */
 /**
- * @author Yogesh Nimbhorkar
+ * The Class RiskControllerTest tests the functionality of the control flow class of Risk Game by the help of Junit.
+ * 
+ * @author Chirag
  *
  */
 package com.soen.risk.test.controller;
