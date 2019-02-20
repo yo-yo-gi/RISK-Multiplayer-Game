2ca3eee7dda138c7702b1d7ed130378d66f02de7
Chirag3112 <46980218+Chirag3112@users.noreply.github.com>
2019-02-20 02:42:02 -0500

    Junit test file added
    junit test file for reinforcement phase

---
+++ Risk/src.test.java/com/soen/risk/test/controller/RiskReinforcementPhaseTest.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ */
+package com.soen.risk.test.controller;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class RiskReinforcementPhaseTest.
+ * @author Chirag
+ * @version 1.0
+ */
+public class RiskReinforcementPhaseTest {
+	
+	/** The x is sample to test the junit functionality */
+	String x= null;
+	
+	/**
+	 * Test.
+	 */
+	@Test
+	public void test() { // sample test method to check the functionality
+		assertNull(x);
+	}
+
+}
