da4b18f158bf9e5b736b2e22efc30a10eac19d21
neha <neha.dighe18@gmail.com>
2019-02-20 19:38:58 -0500

    Junit Test
    Reinforcement Phase Test , Test Suite in process
    20-02-2019

---
+++ Risk/src.test.java/com/soen/risk/test/controller/AllTests.java
@@ -0,0 +1,11 @@
+package com.soen.risk.test.controller;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RiskReinforcementPhaseTest.class })
+public class AllTests {
+
+}
