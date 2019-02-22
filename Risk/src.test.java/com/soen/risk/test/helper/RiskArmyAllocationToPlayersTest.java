92e4610ad0514b028ead1a1b64df39d7bc1c7f4b
Chirag3112 <46980218+Chirag3112@users.noreply.github.com>
2019-02-22 16:22:43 -0500

    Test classes for country and army allocation
    Test classes for country and army allocation

---
+++ Risk/src.test.java/com/soen/risk/test/helper/RiskArmyAllocationToPlayersTest.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ */
+package com.soen.risk.test.helper;
+
+/**
+ * The Class RiskArmyAllocationToPlayersTest.
+ * @author Chirag
+ * @version 1.0
+ */
+public class RiskArmyAllocationToPlayersTest {
+
+}
---
+++ Risk/src.test.java/com/soen/risk/test/helper/RiskTerritoryAssignmentToPlayerTest.java
@@ -0,0 +1,14 @@
+/*
+ * 
+ */
+package com.soen.risk.test.helper;
+
+/**
+ * The Class RiskTerritoryAssignmentToPlayerTest.
+ * @author Chirag
+ * @version 1.0
+ *
+ */
+public class RiskTerritoryAssignmentToPlayerTest {
+
+}
