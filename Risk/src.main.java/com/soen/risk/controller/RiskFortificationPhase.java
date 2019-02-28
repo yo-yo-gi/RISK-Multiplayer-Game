42570a55b8062d97f0c98f30ab22689244222028
Chirag3112 <46980218+Chirag3112@users.noreply.github.com>
2019-02-28 14:24:02 -0500

    fortification phase log addition

--- Risk/src.main.java/com/soen/risk/controller/RiskFortificationPhase.java
+++ Risk/src.main.java/com/soen/risk/controller/RiskFortificationPhase.java
@@ -21,18 +21,19 @@ import com.soen.risk.model.RiskTerritory;
  * @version 1.0
  */
 public class RiskFortificationPhase {
-	
+
+	/** The logger. */
 	//private static final Logger LOGGER = Logger.getLogger(RiskPlayer.class.getName());
 	RiskLogger logger= new RiskLogger();
 	/** The scanner. */
 	Scanner scanner=new Scanner(System.in);
-	
+
 	/** The adjacent territory list. */
 	ArrayList<String> adjTerritoryList;
-	
+
 	/** The destination territory object. */
 	RiskTerritory sourceTerritoryObject,destinationTerritoryObject;
-	
+
 	/**
 	 * Gets the fortified map.
 	 *
@@ -48,32 +49,32 @@ public class RiskFortificationPhase {
 		int sourceTerritory, destinationTerritory, sourceArmy, destinationArmy, sourceIndex, destinationIndex;
 		String sourceTerritoryName, destinationTName;
 		System.out.println(currentPlayer.getPlayerName());
-	
+
 		logger.doLogging("currentPlayer name is: "+currentPlayer);
-		
+
 		do {
-		System.out.println("select the source territory");
-		int sourceTCoutner=1;
-		
-		for (RiskTerritory currTerritory : playerTerritories) {
-			System.out.println(sourceTCoutner+"." + currTerritory.getTerritoryName());
-			sourceTCoutner++;			
-		}
-		sourceTerritory=scanner.nextInt();
-		scanner.nextLine();
-		sourceTerritoryName=playerTerritories.get(sourceTerritory-1).getTerritoryName();
-		sourceArmy=playerTerritories.get(sourceTerritory-1).getArmiesPresent();
-		
+			System.out.println("select the source territory");
+			int sourceTCoutner=1;
+
+			for (RiskTerritory currTerritory : playerTerritories) {
+				System.out.println(sourceTCoutner+"." + currTerritory.getTerritoryName());
+				sourceTCoutner++;			
+			}
+			sourceTerritory=scanner.nextInt();
+			scanner.nextLine();
+			sourceTerritoryName=playerTerritories.get(sourceTerritory-1).getTerritoryName();
+			sourceArmy=playerTerritories.get(sourceTerritory-1).getArmiesPresent();
+
 		}while(!(playerTerritories.get(sourceTerritory-1).getArmiesPresent()>1));
-		
-//		--------------------------------------------------------------------------------
+
+		//		--------------------------------------------------------------------------------
 		adjTerritoryList=new ArrayList<String>();
 		for (RiskTerritory currTerritory : playerTerritories) {			
 			if (currTerritory.getTerritoryName().equalsIgnoreCase(sourceTerritoryName)) {
 				adjTerritoryList=currTerritory.getAdjacents();
 			}
 		}
-//		----------------------------------------------------------------------------------
+		//		----------------------------------------------------------------------------------
 		ArrayList<String> OwnedAdjList=new ArrayList<String>();
 		for (String currAdj : adjTerritoryList) {
 			for (RiskTerritory currTerritory : playerTerritories) {
@@ -82,64 +83,64 @@ public class RiskFortificationPhase {
 				}
 			}
 		}
-		
-	
-		
-//		----------------------------------------------------
-		
+
+
+
+		//		----------------------------------------------------
+
 		if (!OwnedAdjList.isEmpty()) {
-			
-		
-		
-		System.out.println("select the destination territory");
-		int destinationTCoutner=1;
-		for (String currAdj : OwnedAdjList) {
-			System.out.println(destinationTCoutner+"." + currAdj);
-			destinationTCoutner++;			
-		}
-		destinationTerritory=scanner.nextInt();
-		scanner.nextLine();
-		destinationTName=OwnedAdjList.get(destinationTerritory-1);
-		
-		
-//		-------------------------------------------------------------------------------------------------------------------------------
-		
-		for (RiskTerritory currTerritory : playerTerritories) {
-			if (currTerritory.getTerritoryName().equalsIgnoreCase(destinationTName)) {
-				destinationTerritoryObject=currTerritory;
-				destinationArmy=currTerritory.getArmiesPresent();
-				
-			}		
-			if (currTerritory.getTerritoryName().equalsIgnoreCase(sourceTerritoryName)) {
-				sourceTerritoryObject=currTerritory;
-				
+
+
+
+			System.out.println("select the destination territory");
+			int destinationTCoutner=1;
+			for (String currAdj : OwnedAdjList) {
+				System.out.println(destinationTCoutner+"." + currAdj);
+				destinationTCoutner++;			
 			}
+			destinationTerritory=scanner.nextInt();
+			scanner.nextLine();
+			destinationTName=OwnedAdjList.get(destinationTerritory-1);
+
+
+			//		-------------------------------------------------------------------------------------------------------------------------------
+
+			for (RiskTerritory currTerritory : playerTerritories) {
+				if (currTerritory.getTerritoryName().equalsIgnoreCase(destinationTName)) {
+					destinationTerritoryObject=currTerritory;
+					destinationArmy=currTerritory.getArmiesPresent();
+
+				}		
+				if (currTerritory.getTerritoryName().equalsIgnoreCase(sourceTerritoryName)) {
+					sourceTerritoryObject=currTerritory;
+
+				}
+			}
+
+			//		------------------------------------------------------
+			System.out.println("Enter the number of armies to be moved to destination territory:  ");
+			int finalMoveOfArmies = scanner.nextInt();
+			scanner.nextLine();
+			if (finalMoveOfArmies>0 && finalMoveOfArmies<sourceArmy) {
+
+				sourceArmy=sourceTerritoryObject.getArmiesPresent()-finalMoveOfArmies;
+				destinationArmy=destinationTerritoryObject.getArmiesPresent()+finalMoveOfArmies;
+				sourceTerritoryObject.setArmiesPresent(sourceArmy);
+				destinationTerritoryObject.setArmiesPresent(destinationArmy);
+				finalFortifyList.set(playerTerritories.indexOf(sourceTerritoryObject), sourceTerritoryObject);
+				finalFortifyList.set(playerTerritories.indexOf(destinationTerritoryObject), destinationTerritoryObject);
+
+			}else System.out.println("Wrong number of army selected");
+
 		}
-	
-//		------------------------------------------------------
-		System.out.println("Enter the number of armies to be moved to destination territory:  ");
-		int finalMoveOfArmies = scanner.nextInt();
-		scanner.nextLine();
-		if (finalMoveOfArmies>0 && finalMoveOfArmies<sourceArmy) {
-			
-			sourceArmy=sourceTerritoryObject.getArmiesPresent()-finalMoveOfArmies;
-			destinationArmy=destinationTerritoryObject.getArmiesPresent()+finalMoveOfArmies;
-			sourceTerritoryObject.setArmiesPresent(sourceArmy);
-			destinationTerritoryObject.setArmiesPresent(destinationArmy);
-			finalFortifyList.set(playerTerritories.indexOf(sourceTerritoryObject), sourceTerritoryObject);
-			finalFortifyList.set(playerTerritories.indexOf(destinationTerritoryObject), destinationTerritoryObject);
-			
-		}else System.out.println("Wrong number of army selected");
-		
-	}
-		
-		
+
+
 		System.out.println("Fortification complete");
-		
-		
-		
+
+
+
 		fortifiedMap.put(currentPlayer, finalFortifyList);
-		
+
 		return fortifiedMap;
 	}
 }
