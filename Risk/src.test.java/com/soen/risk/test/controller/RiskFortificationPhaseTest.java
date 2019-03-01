18ef07f00ef685ae1b732727b5065708c0b3871c
neha <neha.dighe18@gmail.com>
2019-03-01 17:47:13 -0500

    Risk Fortication Phase Test - Junit
    Junit testing

--- Risk/src.test.java/com/soen/risk/test/controller/RiskFortificationPhaseTest.java
+++ Risk/src.test.java/com/soen/risk/test/controller/RiskFortificationPhaseTest.java
@@ -18,6 +18,7 @@ import com.soen.risk.model.RiskTerritory;
 /**
  * 
  * @author Neha
+ * Fortification done from Venezuela with army 5 to Brazil
  *
  */
 public class RiskFortificationPhaseTest {
@@ -36,13 +37,13 @@ public class RiskFortificationPhaseTest {
 		
 		String[] parsedTerritory = {"Venezuela","South America","Brazil"};
 		RiskTerritory tr1 = new RiskTerritory(parsedTerritory);
-		
+		tr1.setArmiesPresent(5);
 		String[] parsedTerritory1 = {"Brazil","South America","Venezuela","Peru"};
 		RiskTerritory tr2 = new RiskTerritory(parsedTerritory1);
-		
+		tr2.setArmiesPresent(5);
 		String[] parsedTerritory2 = {"Peru","South America","Brazil"};
 		RiskTerritory tr3 = new RiskTerritory(parsedTerritory2);
-		
+		tr3.setArmiesPresent(5);
 		territories.add(tr1);
 		territories.add(tr2);
 		territories.add(tr3);
@@ -51,12 +52,13 @@ public class RiskFortificationPhaseTest {
 		
 		playerTerritoryMap = riskFortificationTest.getFortifiedMap(player1,territories);
 		
-		for (Entry<RiskPlayer, ArrayList<RiskTerritory>> stringListEntry : playerTerritoryMap.entrySet()) {
-			String player = stringListEntry.getKey().getPlayerName();
-			List<RiskTerritory> value = stringListEntry.getValue();
+			List<RiskTerritory> terrirtoryList = playerTerritoryMap.get(player1);
+			Integer []  expected= {3,7};
+			Integer []  actual= {terrirtoryList.get(0).getArmiesPresent(),terrirtoryList.get(1).getArmiesPresent()};
 			
-			assertEquals(1,value.size());
-		}
+			
+			assertArrayEquals(expected, actual);
+		
 	}
 
 }
