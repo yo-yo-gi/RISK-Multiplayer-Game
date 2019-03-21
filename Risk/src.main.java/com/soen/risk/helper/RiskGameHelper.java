2701b0a2b0aa8b91800663f99996db7b094ad088
yo-yo-gi <yogesh007@live.in>
2019-03-21 16:36:39 -0400

    New function to update army after attack
    to use after attack done

--- Risk/src.main.java/com/soen/risk/helper/RiskGameHelper.java
+++ Risk/src.main.java/com/soen/risk/helper/RiskGameHelper.java
@@ -4,7 +4,6 @@
 package com.soen.risk.helper;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,63 +13,61 @@ import com.soen.risk.model.RiskContinent;
 import com.soen.risk.model.RiskPlayer;
 import com.soen.risk.model.RiskTerritory;
 
-
 /**
- * <h2>Game play helper</h2>
- * This class is used for writing utility functions related to game play.
+ * <h2>Game play helper</h2> This class is used for writing utility functions
+ * related to game play.
  *
  * @author Yogesh Nimbhorkar
  * @version 1.0
  */
 public class RiskGameHelper {
 
-
 	/**
 	 * updates existing map player with continents own by players.
 	 *
-	 * @param riskMainMap map of players and territories own by player
+	 * @param riskMainMap       map of players and territories own by player
 	 * @param riskContinentList list of continents objects
-	 * @return controlValueAssignedMap updated map of players and territories own by player
+	 * @return controlValueAssignedMap updated map of players and territories own by
+	 *         player
 	 */
 
 	public LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>> assignControlValuesToPlayer(
 			Map<RiskPlayer, ArrayList<RiskTerritory>> riskMainMap, ArrayList<RiskContinent> riskContinentList) {
 
-		LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>> controlValueAssignedMap= new LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>>();
-		List<RiskContinent> continentList= new ArrayList<RiskContinent>(riskContinentList);
+		LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>> controlValueAssignedMap = new LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>>();
+		List<RiskContinent> continentList = new ArrayList<RiskContinent>(riskContinentList);
 
-		for (Entry<RiskPlayer, ArrayList<RiskTerritory>> entry : riskMainMap.entrySet()){
+		for (Entry<RiskPlayer, ArrayList<RiskTerritory>> entry : riskMainMap.entrySet()) {
 
-			RiskPlayer currentPlayer=entry.getKey();
-			ArrayList<String> currentPlayerOwnedContinents=new ArrayList<String>(currentPlayer.getOccupiedContinents());
-			ArrayList<RiskTerritory> currentPlayerTerritories=new ArrayList<RiskTerritory>(entry.getValue());
-			List<String> currPlayerTerritoriesNames=new ArrayList<String>();
+			RiskPlayer currentPlayer = entry.getKey();
+			ArrayList<String> currentPlayerOwnedContinents = new ArrayList<String>(
+					currentPlayer.getOccupiedContinents());
+			ArrayList<RiskTerritory> currentPlayerTerritories = new ArrayList<RiskTerritory>(entry.getValue());
+			List<String> currPlayerTerritoriesNames = new ArrayList<String>();
 			ArrayList<String> currContTerriNames;
-			
-
 
 			for (RiskTerritory currTerritory : currentPlayerTerritories) {
 				currPlayerTerritoriesNames.add(currTerritory.getTerritoryName());
 			}
 
-			//		Populating currentPlayerOwnedContinents list
+			// Populating currentPlayerOwnedContinents list
 			for (RiskContinent currContinent : continentList) {
-				currContTerriNames=new ArrayList<String>(currContinent.getIncludedTerritories());
+				currContTerriNames = new ArrayList<String>(currContinent.getIncludedTerritories());
 				if (currPlayerTerritoriesNames.containsAll(currContTerriNames)) {
 					currentPlayerOwnedContinents.add(currContinent.getContinentName());
 				}
 			}
-			//		Updating current player list of occupied continents
+			// Updating current player list of occupied continents
 			currentPlayer.setOccupiedContinents(currentPlayerOwnedContinents);
 
-			//		Re populating map with updated player object
-			controlValueAssignedMap.put(currentPlayer, currentPlayerTerritories);			 
+			// Re populating map with updated player object
+			controlValueAssignedMap.put(currentPlayer, currentPlayerTerritories);
 
 		}
 
 		return controlValueAssignedMap;
 	}
-	
+
 	/**
 	 * Calculate initial armies.
 	 *
@@ -84,7 +81,7 @@ public class RiskGameHelper {
 		if (numOfPlayers == 3)
 			armies = 5;
 		else if (numOfPlayers == 4)
-			armies = 30; 
+			armies = 30;
 		else if (numOfPlayers == 5)
 			armies = 25;
 		else if (numOfPlayers == 6)
@@ -92,7 +89,7 @@ public class RiskGameHelper {
 
 		return armies;
 	}
-	
+
 	/**
 	 * This method calculates map control for the domination view
 	 * 
@@ -102,38 +99,71 @@ public class RiskGameHelper {
 	 */
 	public static String calculateDominationMapControlled(long totalTerritories, long ownedTerritories) {
 		String mapControll = null;
-		
-		long calculation = Math.round((totalTerritories/ownedTerritories)*100.0);
-		mapControll=Long.toString(calculation);
+
+		long calculation = Math.round((totalTerritories / ownedTerritories) * 100.0);
+		mapControll = Long.toString(calculation);
 		return mapControll;
 	}
-	
+
 	/**
 	 * Finds the territory by name to use in attack phase
 	 * 
-	 * @param riskMap map of player
+	 * @param riskMap       map of player
 	 * @param territoryName name of territory to search
 	 * @return riskTerritory territory to be returned
 	 */
-	public static RiskTerritory getRiskTerritoryByName(Map<RiskPlayer, ArrayList<RiskTerritory>> riskMap, String territoryName) {
+	public static RiskTerritory getRiskTerritoryByName(Map<RiskPlayer, ArrayList<RiskTerritory>> riskMap,
+			String territoryName) {
 		RiskTerritory riskTerritory = new RiskTerritory();
-		ArrayList<RiskTerritory> territoryList=new ArrayList<RiskTerritory>();
-		boolean foundFlag=false;
-		for (Entry<RiskPlayer, ArrayList<RiskTerritory>> entry : riskMap.entrySet()){
+		ArrayList<RiskTerritory> territoryList = new ArrayList<RiskTerritory>();
+		boolean foundFlag = false;
+		for (Entry<RiskPlayer, ArrayList<RiskTerritory>> entry : riskMap.entrySet()) {
 			territoryList.addAll(entry.getValue());
 		}
-		
+
 		for (RiskTerritory currTerritory : territoryList) {
 			if (currTerritory.getTerritoryName().equalsIgnoreCase(territoryName)) {
-				riskTerritory=currTerritory;
-				foundFlag=true;
+				riskTerritory = currTerritory;
+				foundFlag = true;
 			}
 		}
-		
+
 		if (!foundFlag) {
 			riskTerritory.setArmiesPresent(-1);
 		}
-		
+
 		return riskTerritory;
+	}
+
+	/**
+	 * Method to update army after attack
+	 * 
+	 * @param attackSourceTerritory source territory of attack
+	 * @param attackDestinationTerritory destination territory of attack
+	 * @param riskMainMap Map of players with respective territory
+	 * @return updatedMap Updated map after army updation
+	 */
+	public static LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>> updateArmyAfterAttack(
+			RiskTerritory attackSourceTerritory, RiskTerritory attackDestinationTerritory,
+			LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>> riskMainMap) {
+		
+		LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>> updatedMap=new LinkedHashMap<RiskPlayer, ArrayList<RiskTerritory>>(riskMainMap);
+		for (Entry<RiskPlayer, ArrayList<RiskTerritory>> entry : riskMainMap.entrySet()) {
+
+			for (RiskTerritory currRiskTerritory : entry.getValue()) {
+				ArrayList<RiskTerritory> tempTerritoriesList=new ArrayList<RiskTerritory>(entry.getValue());
+				if (currRiskTerritory.getTerritoryName().equalsIgnoreCase(attackSourceTerritory.getTerritoryName())) {
+					tempTerritoriesList.set(entry.getValue().indexOf(currRiskTerritory), attackSourceTerritory);
+					updatedMap.put(entry.getKey(), tempTerritoriesList);
+				}
+				if (currRiskTerritory.getTerritoryName().equalsIgnoreCase(attackDestinationTerritory.getTerritoryName())) {
+					tempTerritoriesList.set(entry.getValue().indexOf(currRiskTerritory), attackDestinationTerritory);
+					updatedMap.put(entry.getKey(), tempTerritoriesList);
+				}
+			}
+
+		}
+
+		return updatedMap;
 	}
 }
