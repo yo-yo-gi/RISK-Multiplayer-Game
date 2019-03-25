6866bd0efeaa24c244eb289372d345c6b4bf31b9
Chirag3112 <46980218+Chirag3112@users.noreply.github.com>
2019-03-24 20:48:50 -0400

    Junits for the the attack phase and gameHelper
    Junits for attack phase and gameHelper class

--- Risk/src.test.java/com/soen/risk/test/controller/RiskAttackPhaseTest.java
+++ Risk/src.test.java/com/soen/risk/test/controller/RiskAttackPhaseTest.java
@@ -1,3 +1,6 @@
+/*
+ * 
+ */
 package com.soen.risk.test.controller;
 
 import static org.junit.Assert.assertNotNull;
@@ -10,12 +13,29 @@ import org.junit.Test;
 
 import com.soen.risk.controller.RiskAttackPhase;
 
+/**
+ * The Class RiskAttackPhaseTest is Junit class to test the dice roll functionalities of the attack phase.
+ * 
+ * @author Chirag
+ * @version 2.0
+ */
 public class RiskAttackPhaseTest {
+	
+	/** The output. */
 	Map<String,Object> output=new HashMap<String,Object>();
+	
+	/** The attack destination territory name. */
 	String attackSourceTerritoryName, attackDestinationTerritoryName;
+	
+	/** The defending army count. */
 	int attackingArmyCount, defendingArmyCount;
+	
+	/** The risk attack phase. */
 	RiskAttackPhase riskAttackPhase = new RiskAttackPhase();
 
+	/**
+	 * Sets the data for the test methods.
+	 */
 	@Before
 	public void setUp() {
 		attackSourceTerritoryName="Alaska";
@@ -24,12 +44,18 @@ public class RiskAttackPhaseTest {
 		defendingArmyCount=3;
 	}
 
+	/**
+	 * Test roll dice for normal attack mode.
+	 */
 	@Test
 	public void testRollDiceForNormalAttackMode() {
 		output=riskAttackPhase.rollDiceForNormalAttackMode(attackSourceTerritoryName, attackingArmyCount, attackDestinationTerritoryName, defendingArmyCount);
 		assertNotNull(output);
 	}
 
+	/**
+	 * Test roll dice for all out attack mode.
+	 */
 	@Test
 	public void testRollDiceForAllOutAttackMode() {
 		output=RiskAttackPhase.rollDiceForAllOutAttackMode(attackSourceTerritoryName, attackingArmyCount, attackDestinationTerritoryName, defendingArmyCount);
