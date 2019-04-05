5c7286f8e81cdcc191817d96d63aa9f3bfec11e8
Shashank Rao <shashankrao1994@gmail.com>
2019-04-05 16:58:43 -0400

    RiskRandomStrategy update

--- Risk/src.main.java/com/soen/risk/startegies/RiskRandomStrategy.java
+++ Risk/src.main.java/com/soen/risk/startegies/RiskRandomStrategy.java
@@ -3,6 +3,7 @@
  */
 package com.soen.risk.startegies;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -13,6 +14,7 @@ import java.util.Map.Entry;
 import com.soen.risk.controller.RiskAttackPhase;
 import com.soen.risk.helper.RiskAttackHelper;
 import com.soen.risk.helper.RiskGameHelper;
+import com.soen.risk.helper.RiskReinforcementHelper;
 import com.soen.risk.model.RiskContinent;
 import com.soen.risk.model.RiskPhase;
 import com.soen.risk.model.RiskPhaseType;
@@ -23,9 +25,13 @@ import com.soen.risk.model.RiskTerritory;
  * @author SHASHANK RAO
  *
  */
-public class RiskRandomStrategy implements RiskPlayerStrategy {
+public class RiskRandomStrategy implements RiskPlayerStrategy , Serializable{
 
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 701482605691028024L;
 	RiskPlayer currentPlayer;
 	RiskTerritory reinforcementTerritory;
 	RiskTerritory attackTerritory;
